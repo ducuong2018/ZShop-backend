c19ed6d52ddd56b29c1d9e12d94b8dd44edfa954
Cuong DC <ducuong2017@gmail.com>
2021-06-06 02:52:03 +0700

    no messsage

--- src/main/java/com/example/zshop/repositories/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.zshop.repositories;
-
-import com.example.zshop.entities.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-}
