5ae1a5008a2beb32a291934ac207e449694807a1
Cuong DC <ducuong2017@gmail.com>
2021-07-12 00:37:20 +0700

    .

--- file1.sql
+++ file1.sql
@@ -1,13 +1,24 @@
-
-create table products.products(
-                                  id serial primary key,
-                                  user_id serial,
-                                  name varchar(50),
-                                  image text,
-                                  price decimal,
-                                  price_sale decimal,
-                                  description text,
-                                  quantity serial,
-
-                                  CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES accounts.user(id)
-)
+create schema accounts;
+create table accounts.user(
+                              id serial primary key,
+                              email varchar unique,
+                              phone_number varchar unique,
+                              password_crypt text,
+                              profile jsonb,
+                              created_at timestamp without time zone default CURRENT_TIMESTAMP,
+                              updated_at timestamp without time zone default CURRENT_TIMESTAMP
+);
+create schema products
+    create table products.products(
+                                      id serial primary key,
+                                      user_id serial,
+                                      name varchar(50),
+                                      image text,
+                                      price decimal,
+                                      price_sale decimal,
+                                      description text,
+                                      quantity serial,
+                                      CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES accounts.user(id),
+                                      created_at timestamp without time zone default CURRENT_TIMESTAMP,
+                                      updated_at timestamp without time zone default CURRENT_TIMESTAMP
+    )
